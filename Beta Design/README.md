a10e4b704ae20a29e81df194bf40a87f6c84d8cd
LZ27@proton.me <LZT7@proton.me>
2024-01-09 21:16:03 -0600

    Functioning Design
    will improve later

--- Beta Design/README.md
+++
@@ -1,2 +0,0 @@
-# ElementBuild.github.io
-Website
--- script.js
+++ script.js
@@ -10,9 +10,35 @@ const mNothing = document.getElementById('mNothing');
 mHome.addEventListener('click', () => { sendUser("index", "Home Page Button"); });
 mButtons.addEventListener('click', () => { sendUser("buttons", "Buttons Page Button") });
 mInputs.addEventListener('click', () => { sendUser("inputs", "Inputs Page Button"); });
-mNothing.addEventListener('click', () => { sendUser("nothing", "Nothing Page Button"); });
+// mNothing.addEventListener('click', () => { sendUser("nothing", "Nothing Page Button"); });
+mNothing.addEventListener('click', () => { document.body.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQIFTc5h-Shyc7HTDy8I20Wyb1YtpCn7v_yo6H4NSSPQ&s)" ;});
 
 function sendUser(location, log){
     window.location.href = './' + location + '.html';
     console.log(log + ' clicked!');
 }
+
+
+// 
+const live = document.getElementById("live");
+const makecolor = document.querySelector(".xC");
+// const makered = document.querySelector(".xB");
+const maketext = document.querySelector(".xT");
+const makebradius = document.querySelector(".xR");
+
+/* makered.addEventListener('click', () => { 
+    live.style.backgroundColor = "red";
+    makecolor.value = "#ff0000";
+}); */
+
+makecolor.addEventListener('input', () => { 
+    live.style.backgroundColor = makecolor.value;
+});
+
+maketext.addEventListener('input', () => { 
+    live.innerText = maketext.value;
+});
+
+makebradius.addEventListener('input', () => { 
+    live.style.borderRadius = makebradius.value + "px";
+});
